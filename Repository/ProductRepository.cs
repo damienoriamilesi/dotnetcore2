6f92b26c5dfc00438227d1178a16f11fe5d949e2
damien <damien.oria@gmail.com>
2018-08-12 17:25:06 +0200

    Docker / Docker compose comments

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -1,9 +1,27 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
 using coreapp.Data;
-public class ProductRepository : Repository<Product>{
-    // private readonly DutchContext _ctx;
+public class ProductRepository : Repository<Product>
+{
+    private readonly DutchContext _ctx;
 
-    // public ProductRepository(DutchContext ctx)
-    // {
-    //     _ctx = ctx;
-    // }
+    public ProductRepository(DutchContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public void GetData()
+    {
+        var ids = new []{ "1","2","3","4" };
+        var query = ids.Where(GetPredicate);
+    }
+
+    public bool GetPredicate(string x)
+    {
+        var predicate = x == "1";
+        predicate = predicate && x == "2";
+
+        return predicate;
+    }
 }
